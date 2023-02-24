147086b6d18e6cb03b05a4bff7c28a1bae85897c
Alexandra Alvarez <alexandraalvarez@MacBook-Air-de-Alexandra.local>
2023-02-23 22:30:22 -0400

    modificaciones en las extensiones

--- domain.js
+++ domain.js
@@ -1,13 +1,16 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let ext = [".com", ".net", ".es" ];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      document.write((pronoun[i])+(adj[j])+(noun[k]) + ".com <br>" );
+    for (let k = 0; k < noun.length; k++) 
+      for (let l = 0; l < ext.length; l++) 
+    {
+      document.write((pronoun[i])+(adj[j])+(noun[k])+(ext[l]) + "<br>" );
 
-      console.log((pronoun[i])+(adj[j])+(noun[k]) + ".com" );
+      console.log((pronoun[i])+(adj[j])+(noun[k])+(ext[l]));
     }
   }
 }
